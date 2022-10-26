36284994be25bdfcdf7d08f44553ad3d4cb9b20d
Oscar Tinajero <tinajero117.oscar@gmail.com>
2022-10-26 17:18:45 -0500

    Test changes

--- test/image_watermark_test.dart
+++ test/image_watermark_test.dart
@@ -4,6 +4,6 @@ import 'package:image_watermark/image_watermark.dart';
 
 void main() {
   test('adds one to input values', () {
-    final calculator = image_watermark();
+    final calculator = ImageWatermark();
   });
 }
